621811431ee3b95726808b0d1685f07f7b439e87
Md. Zakir Al Hasan <zakiralhasan.bd@gmail.com>
2023-07-19 14:53:52 +0600

    scroll down section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,21 @@
 
 import Header from '../../components/header/Header';
 
+import { BsMouse } from 'react-icons/bs';
 const Home = () => {
     return (
         <>
-            <Header></Header>
+            <div id='home'>
+                <div className='home-container'>
+                    <div className='home-page-inner'>
+                        <Header></Header>
+                        <div className='flex flex-col items-center justify-center gap-3'>
+                            <p>SCROLL DOWN</p>
+                            <BsMouse className='text-6xl' />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
